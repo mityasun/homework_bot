ab14e2771367d413a20596884fcfae72313e0418
unknown <mityasun@gmail.com>
2022-07-05 11:50:13 +0500

    Спринт 7. Проект после 2го ревью.

--- homework.py
+++ homework.py
@@ -16,7 +16,7 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 
-RETRY_TIME = 10
+RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
