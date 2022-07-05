d2cd07ada05efdd725320a576ea47e414e7a8597
unknown <mityasun@gmail.com>
2022-07-05 11:48:45 +0500

    Спринт 7. Проект после 2го ревью.

--- exceptions.py
+++ exceptions.py
@@ -1,8 +1,22 @@
 class WrongResponseCode(Exception):
     """Неверный ответ API."""
+
     pass
 
 
 class NotForSend(Exception):
     """Исключение не для пересылки в telegram."""
+
+    pass
+
+
+class EmptyResponseFromAPI(NotForSend):
+    """Пустой ответ API."""
+
+    pass
+
+
+class TelegramError(NotForSend):
+    """Ошибка отправки сообщения в telegram."""
+
     pass
